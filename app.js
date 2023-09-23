547600677ce4abe20272d268d9f136ae1f908cab
Mmasechaba Francesca Seopa <44195461+charbileigh@users.noreply.github.com>
2023-09-23 16:36:16 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+
+const today = new Date();
+
+console.log(today);
+
+const weeksDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", 
+"September", "October", "November", "December"];
+
+
+date.innerHTML = today.getDate();
+day.innerHTML = weeksDays[today.getDay()];
+month.innerHTML = allMonths[today.getMonth()];
+year.innerHTML = today.getFullYear();
+
+
